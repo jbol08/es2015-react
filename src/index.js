21c2881a6d5b4d3774594f55dd505b566d90eff3
Jose Bolanos <bolanosjjr@gmail.com>
2021-03-04 14:20:44 -0600

    es2015 w/react prac

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import fruits from './foods';
+import { choice, remove } from './helpers';
+
+let fruit = choice(fruits)
+console.log(`I'd like one ${fruit}, please.`)
+console.log(`Here you go: ${fruit}`)
+console.log('Delicious! May I have another?')
+let leftOver = remove(fruit, fruits);
+console.log(`I'm sorry, we're all out. We have ${leftOver.length} left.`)
+
+
